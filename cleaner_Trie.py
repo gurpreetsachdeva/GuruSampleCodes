3866c092e857311c7e4200cd4634a6db43493df0
Gurpreet Sachdeva <gurpreetsachdeva@Gurpreets-MacBook-Air.local>
2019-11-17 12:32:56 +0530

    Find a better optimization for figuring out bad set

--- cleaner_Trie.py
+++ cleaner_Trie.py
@@ -1,16 +1,15 @@
 #Problem Solving : For a bunch words print bad set if anyone is a prefix of anyone
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 import sys
-array_length=input()
-words=[]
-output=["BAD SET","GOOD SET"]
-for i in range(len(array_length)):
-    words.append(input())
+
 
 class Node:
     def __init__(self):
         self.character_map={}
         self.leaf_node=False
+        self.value=0
 class Trie:
     def __init__(self):
         self.root=Node()
@@ -21,6 +20,7 @@ class Trie:
             if(top_node.character_map.get(ch,None)==None):
                 top_node.character_map[ch]=Node()
             top_node=top_node.character_map[ch]
+            top_node.value+=1
         top_node.leaf_node=True
     def insertWords(self,words):
         for w in words:
@@ -35,8 +35,8 @@ class Trie:
             if(top_node.character_map.get(ch,None)==None):
                 return 0 #directly return zero or len([]) 
             top_node=top_node.character_map[ch]
-        ans=self.subtree_iterate(top_node,search_word)
-        return len(ans) ## or In Java ans.size() ;)
+        ans=top_node.value
+        return ans ## or In Java ans.size() ;)
     def subtree_iterate(self,level_node,search_word):
         ans=[]
         if(level_node.leaf_node==True):
@@ -44,19 +44,27 @@ class Trie:
         for key,value in level_node.character_map.items():
             ans.extend(self.subtree_iterate(value,search_word+key))
         return ans #Return all the prefix matches , auto_completion ;)
-if __name__=='__main__':   
+if __name__=='__main__':
+    array_length=int(input())
+    #print("Array length"+str(array_length))
+    words=[]
+    output=["BAD SET","GOOD SET"]
+    for i in range(array_length):
+        words.append(input().rstrip())   
     trie=Trie()
-
-    print(words)
-    for word in words:
-        print("Starting for word"+word)
-        print(trie.prefixString(''))
-        print(word +"Searching word"+str(trie.prefixString(word)>0))
-        if(trie.prefixString(word)>0):
-            print (output[0])
-            print(word)
-            sys.exit(0)
-        trie.insertWord(word)
+    #words=['aab','aac','aacghgh','aabghgh']
+    #print(words)
+    for i in range(0,len(words)):
+        #print("Inserting "+words[i])
+        trie.insertWord(words[i])
+        for j in range(0,i+1):
+            if trie.prefixString(words[j])>1:
+                print(output[0])
+                #print("never in break")
+                print(words[i])
+                sys.exit(0)
+                
+        
         
     print(output[1])
 
