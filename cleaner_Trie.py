f485475d43e11370700e6084c796029dd70738f7
Gurpreet Sachdeva <gurpreetsachdeva@Gurpreets-MacBook-Air.local>
2019-11-17 13:28:02 +0530

    Optimized the insert to figure out same prefix insertion

--- cleaner_Trie.py
+++ cleaner_Trie.py
@@ -1,7 +1,5 @@
 #Problem Solving : For a bunch words print bad set if anyone is a prefix of anyone
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-
-# Enter your code here. Read input from STDIN. Print output to STDOUT
 import sys
 
 
@@ -19,8 +17,12 @@ class Trie:
             #character not already there, then add , otherwise just iterate
             if(top_node.character_map.get(ch,None)==None):
                 top_node.character_map[ch]=Node()
-            top_node=top_node.character_map[ch]
             top_node.value+=1
+            top_node=top_node.character_map[ch]
+            if(top_node.leaf_node==True):
+                return True
+        if(len(top_node.character_map)>0):
+            return True
         top_node.leaf_node=True
     def insertWords(self,words):
         for w in words:
@@ -35,8 +37,8 @@ class Trie:
             if(top_node.character_map.get(ch,None)==None):
                 return 0 #directly return zero or len([]) 
             top_node=top_node.character_map[ch]
-        ans=top_node.value
-        return ans ## or In Java ans.size() ;)
+        ans=self.subtree_iterate(top_node,search_word)
+        return len(ans) ## or In Java ans.size() ;)
     def subtree_iterate(self,level_node,search_word):
         ans=[]
         if(level_node.leaf_node==True):
@@ -46,24 +48,17 @@ class Trie:
         return ans #Return all the prefix matches , auto_completion ;)
 if __name__=='__main__':
     array_length=int(input())
-    #print("Array length"+str(array_length))
     words=[]
     output=["BAD SET","GOOD SET"]
     for i in range(array_length):
         words.append(input().rstrip())   
     trie=Trie()
-    #words=['aab','aac','aacghgh','aabghgh']
-    #print(words)
-    for i in range(0,len(words)):
-        #print("Inserting "+words[i])
-        trie.insertWord(words[i])
-        for j in range(0,i+1):
-            if trie.prefixString(words[j])>1:
-                print(output[0])
-                #print("never in break")
-                print(words[i])
-                sys.exit(0)
-                
+    for word in words:
+        flag=trie.insertWord(word)
+        if(flag):
+            print (output[0])
+            print(word)
+            sys.exit(0)
         
         
     print(output[1])
