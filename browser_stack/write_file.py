02ba2173abca4fa4301c7c8860a09642e6839ea7
Gurpreet <gurpreet.sachdevagss@yahoo.in>
2019-12-04 17:32:03 +0530

    Full commit

--- browser_stack/write_file.py
+++
@@ -1,4 +0,0 @@
-with open("my_log.txt","r+") as f:
-	for i in range(10000000):
-		f.write(str(i)+"\n")
-
--- merge.py
+++ merge.py
@@ -22,3 +22,27 @@ def merge(arr1,arr2):
 a=[1,2,3]
 b=[-1,4,5,0,0,0]
 merge(a,b)
+
+
+def findindex(arr,start,end):
+	#print(start,end)
+	if(start>end):
+		return
+	mid=int((start+end)/2)
+	if(end<mid+1):
+		return -1	
+	if(arr[mid]>arr[mid+1]):
+		#print("xxx")
+		return mid+1
+	if(arr[start]<arr[mid]):
+		return findindex(arr,mid,end)
+	else:
+		return findindex(arr,start,mid)
+x=[i for i in range(10000)]
+x.append(-1)
+x.append(-2)
+print(findindex(x,0,len(x)-1))
+print("second test case")
+x=[i for i in range(10000)]
+print(findindex(x,0,len(x)-1))
+
